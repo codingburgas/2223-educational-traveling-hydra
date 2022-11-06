65b010cc917d9f3d87fba57acb454ad884c53c34
GAIvanova20 <GAIvanova20@codingburgas.bg>
2022-11-06 19:39:45 +0200

    Position the card deck. Close #4

--- Around Europe/_build/main.cpp
+++ Around Europe/_build/main.cpp
@@ -1,19 +1,25 @@
-#include "raylib.h"
+#include "main.h"
 
 int main()
 {
     InitWindow(1500, 945, "Around Europe");
 
     Texture2D map = LoadTexture("../resources/map.png");
+    Texture2D cards = LoadTexture("../resources/cards.png");
+    
+    Rectangle deck = { 0, 0, (float)(cards.width / 6), (float)cards.height};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
         DrawTextureEx(map, { 0, 0 }, 0, 1, WHITE);
+        DrawTextureRec(cards, deck, {620, 760}, WHITE);
         
         EndDrawing();
     }
+    UnloadTexture(map);
+    UnloadTexture(cards);
 
     CloseWindow();
 
--- Around Europe/_build/playerInfo.h
+++ Around Europe/_build/playerInfo.h
@@ -1,5 +1,5 @@
 #include <iostream>
 
-using namespce std;
+using namespace std;
 
 void showInfo();
